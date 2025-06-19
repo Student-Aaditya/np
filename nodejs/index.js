890c36bbce659dc011f1e676bb28dddac390eb70
Aaditya <aadityakuamr518@gmail.com>
2025-06-19 23:39:16 +0530

    updated url

--- nodejs/index.js
+++ nodejs/index.js
@@ -6,7 +6,7 @@ app.use(express.json());
 
 app.get('/', async (req, res) => {
   try {
-    const response = await axios.post('http://localhost:5000/predict', {
+    const response = await axios.post('https://np-clwb.onrender.com/predict', {
       name: 'Aaditya'
     });
 
